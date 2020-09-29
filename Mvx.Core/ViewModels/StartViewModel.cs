65a1f3a342d011208e1cc3c14fc5348e28d47d49
TheMrChristli <christian-wenzl96@web.de>
2020-09-29 15:54:15 +0200

    * Redesign Base Layout

---
+++ Mvx.Core/ViewModels/StartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mvx.Core.ViewModels
+{
+    public class StartViewModel : MvxViewModel
+    {
+    }
+}
---
+++ Mvx.UWP/Views/StartView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MvvmCross.Platforms.Uap.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace Mvx.UWP.Views
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class StartView : MvxWindowsPage
+    {
+        public StartView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

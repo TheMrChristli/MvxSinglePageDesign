f39e2019d42f9897d6cd6be53be467562bd5a786
TheMrChristli <christian-wenzl96@web.de>
2021-01-18 23:45:16 +0100

    Add Playground

--- Mvx.WPF/Views/StartView.xaml.cs
+++ Mvx.WPF/Views/StartView.xaml.cs
@@ -1,15 +1,27 @@
-﻿using MvvmCross.Platforms.Wpf.Views;
+﻿using MvvmCross.Platforms.Wpf.Presenters.Attributes;
+using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.Presenters;
+using MvvmCross.Presenters.Attributes;
+using MvvmCross.ViewModels;
 
 namespace Mvx.WPF.Views
 {
     /// <summary>
     /// Interaktionslogik für StartView.xaml
     /// </summary>
-    public partial class StartView : MvxWpfView
+    public partial class StartView : IMvxOverridePresentationAttribute
     {
         public StartView()
         {
             InitializeComponent();
+        }
+
+        public MvxBasePresentationAttribute PresentationAttribute(MvxViewModelRequest request)
+        {
+            return new MvxWindowPresentationAttribute
+            {
+                ViewType = typeof(FirstView)
+            };
         }
     }
 }

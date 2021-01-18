6fdfa44c34eb55eadf93e7be8cda380df62144e4
TheMrChristli <christian-wenzl96@web.de>
2021-01-18 21:23:55 +0100

    Add WPF Playground

--- Mvx.WPF/Views/FirstView.xaml.cs
+++ Mvx.WPF/Views/FirstView.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.Platforms.Wpf.Presenters.Attributes;
 
 namespace Mvx.WPF.Views
 {
     /// <summary>
     /// Interaktionslogik für FirstView.xaml
     /// </summary>
+    [MvxContentPresentation]
     public partial class FirstView : MvxWpfView
     {
         public FirstView()
--- Mvx.WPF/Views/SecondView.xaml.cs
+++ Mvx.WPF/Views/SecondView.xaml.cs
@@ -1,10 +1,12 @@
-﻿using MvvmCross.Platforms.Wpf.Views;
+﻿using MvvmCross.Platforms.Wpf.Presenters.Attributes;
+using MvvmCross.Platforms.Wpf.Views;
 
 namespace Mvx.WPF.Views
 {
     /// <summary>
     /// Interaktionslogik für SecondView.xaml
     /// </summary>
+    [MvxContentPresentation]
     public partial class SecondView : MvxWpfView
     {
         public SecondView()

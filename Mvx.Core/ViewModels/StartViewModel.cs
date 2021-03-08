1794e9d390dac043b4d2a6ae9fe60ef4138c1e84
Christian Wenzl <christian-wenzl96@web.de>
2021-03-08 18:06:58 +0100

    Remove unnessesary methods

--- Mvx.Core/ViewModels/StartViewModel.cs
+++ Mvx.Core/ViewModels/StartViewModel.cs
@@ -15,25 +15,8 @@ namespace Mvx.Core.ViewModels
             ShowSecondViewCommand = new MvxAsyncCommand(async () => await NavigationService.Navigate<SecondViewModel>());
         }
 
-        public override async void ViewAppeared()
-        {
-            await Task.Delay(1);
-            //await NavigationService.Navigate<FirstViewModel>();
-        }
-
         public MvxAsyncCommand ShowFirstViewCommand { get; private set; }
 
         public MvxAsyncCommand ShowSecondViewCommand { get; private set; }
-
-
-        protected override void SaveStateToBundle(IMvxBundle bundle)
-        {
-            base.SaveStateToBundle(bundle);
-        }
-
-        protected override void ReloadFromBundle(IMvxBundle state)
-        {
-            base.ReloadFromBundle(state);
-        }
     }
 }
